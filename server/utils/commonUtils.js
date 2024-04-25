1ed7f22f0021b9a75b4be3feafb45dd95400b797
Kyrylo <fresh.js.ftd.@gmail.com>
2024-04-25 17:49:11 +0300

    Add promisify examples

--- server/utils/commonUtils.js
+++ server/utils/commonUtils.js
@@ -19,3 +19,82 @@ function myPromisify(funcWithAsyncCallback) {
     });
   }
 }
+
+function myPromisify2(funcWithAsyncCallback) {
+  // повертаємо функцію яка повертає проміс і приймає аргументи початкової функції
+  return function promisifiedFunc (...args) {
+
+    return new Promise((resolve, reject) => {
+      // запускаємо початкову функцію та передаємо їй аргументи, після чого окремо передємо коллбек
+      funcWithAsyncCallback(...args, (data, err ) => {
+        if(err) {
+          // якщо є помилка то проміс відхиляємо з нею
+          reject(err);
+        } else {
+          // якщо немає помилки то проміс приймаємо з даними
+          resolve(data);
+        }
+      });
+    });
+  }
+}
+
+
+function myPromisify3(funcWithAsyncCallback) {
+  // повертаємо функцію яка повертає проміс і приймає аргументи початкової функції
+  return function promisifiedFunc (...args) {
+
+    return new Promise((resolve, reject) => {
+      // запускаємо початкову функцію та передаємо їй аргументи, після чого окремо передємо коллбек
+      funcWithAsyncCallback((data, err ) => {
+        if(err) {
+          // якщо є помилка то проміс відхиляємо з нею
+          reject(err);
+        } else {
+          // якщо немає помилки то проміс приймаємо з даними
+          resolve(data);
+        }
+      });
+    }, ...args );
+  }
+}
+
+
+
+/**
+ * 
+ * @param {string} name 
+ * @param {number} number 
+ * @param {(err, text) => {}} callback 
+ */
+const myBadAsyncFunc = (name, number, callback) => {
+
+  const isNameGiven = name.trim() !== '' && typeof name === 'string';
+
+  const isNumberGiven = typeof number === 'number' && !isNaN(number);
+
+  const error = isNameGiven && isNumberGiven ? null : new Error('Invalid data given');
+
+  const text = `Name is ${name} and number is ${number}`;
+
+  callback(error, text);
+}
+
+// myBadAsyncFunc('Test', 1234, (err, text) => {
+//   if(err) throw err;
+
+//   console.log(text);
+// });
+
+async function testMyPromisify () {
+  const promisified = myPromisify(myBadAsyncFunc);
+
+  try {
+    const text = await promisified('', NaN);
+    console.log(text);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+testMyPromisify();
